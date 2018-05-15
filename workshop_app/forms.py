e30380ac6df30f816a2bc91c9dd4f13f22084950
Akshen <doke.akshen@gmail.com>
2018-05-15 10:28:30 +0530

    Fix choices in forms.py

--- workshop_app/forms.py
+++ workshop_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils import timezone
 from .models import (
-                    Profile, User, Workshop, WorkshopType, 
+                    Profile, User, Workshop, WorkshopType,
                     RequestedWorkshop, BookedWorkshop, ProposeWorkshopDate
                     )
 from string import punctuation, digits
@@ -34,7 +34,6 @@ department_choices = (
     ("biosciences and bioengineering", "Biosciences and  BioEngineering"),
     ("electronics", "Electronics"),
     ("energy science and engineering", "Energy Science and Engineering"),
-    ("others", "Others"),
     )
 
 title = (
@@ -46,56 +45,53 @@ title = (
     ("Mr", "Mr."),
     ("Mrs", "Mrs."),
     ("Miss", "Ms."),
-    ("other", "Other"),
     )
 
 source = (
-    ("FOSSEE fellowship", "FOSSEE fellowship"),
     ("FOSSEE Email", "FOSSEE Email"),
     ("FOSSEE website", "FOSSEE website"),
     ("Google", "Google"),
     ("Social Media", "Social Media"),
     ("From other College", "From other College"),
-    ("Others", "Others"),
     )
 
 states = (
-    ("IN-AP",	"Andhra Pradesh"),	
-    ("IN-AR",	"Arunachal Pradesh"),	
-    ("IN-AS",	"Assam"),	
-    ("IN-BR",	"Bihar"),	
-    ("IN-CT",	"Chhattisgarh"),	
+    ("IN-AP",	"Andhra Pradesh"),
+    ("IN-AR",	"Arunachal Pradesh"),
+    ("IN-AS",	"Assam"),
+    ("IN-BR",	"Bihar"),
+    ("IN-CT",	"Chhattisgarh"),
     ("IN-GA",	"Goa"),	
     ("IN-GJ",	"Gujarat"),	
-    ("IN-HR",	"Haryana"),	
-    ("IN-HP",	"Himachal Pradesh"),	
-    ("IN-JK",	"Jammu and Kashmir"),	
-    ("IN-JH",	"Jharkhand"),	
-    ("IN-KA",	"Karnataka"),	
-    ("IN-KL",	"Kerala"),	
-    ("IN-MP",	"Madhya Pradesh"),	
-    ("IN-MH",	"Maharashtra"),	
-    ("IN-MN",	"Manipur"),	
-    ("IN-ML",	"Meghalaya"),	
-    ("IN-MZ",	"Mizoram"),	
-    ("IN-NL",	"Nagaland"),	
-    ("IN-OR",	"Odisha"),	
-    ("IN-PB",	"Punjab"),	
-    ("IN-RJ",	"Rajasthan"),	
-    ("IN-SK",	"Sikkim"),	
-    ("IN-TN",	"Tamil Nadu"),	
-    ("IN-TG",	"Telangana"),	
+    ("IN-HR",	"Haryana"),
+    ("IN-HP",	"Himachal Pradesh"),
+    ("IN-JK",	"Jammu and Kashmir"),
+    ("IN-JH",	"Jharkhand"),
+    ("IN-KA",	"Karnataka"),
+    ("IN-KL",	"Kerala"),
+    ("IN-MP",	"Madhya Pradesh"),
+    ("IN-MH",	"Maharashtra"),
+    ("IN-MN",	"Manipur"),
+    ("IN-ML",	"Meghalaya"),
+    ("IN-MZ",	"Mizoram"),
+    ("IN-NL",	"Nagaland"),
+    ("IN-OR",	"Odisha"),
+    ("IN-PB",	"Punjab"),
+    ("IN-RJ",	"Rajasthan"),
+    ("IN-SK",	"Sikkim"),
+    ("IN-TN",	"Tamil Nadu"),
+    ("IN-TG",	"Telangana"),
     ("IN-TR",	"Tripura"),
-    ("IN-UT",	"Uttarakhand"),	
-    ("IN-UP",	"Uttar Pradesh"),	
+    ("IN-UT",	"Uttarakhand"),
+    ("IN-UP",	"Uttar Pradesh"),
     ("IN-WB",	"West Bengal"),
     ("IN-AN",	"Andaman and Nicobar Islands"),	
-    ("IN-CH",	"Chandigarh"),	
-    ("IN-DN",	"Dadra and Nagar Haveli"),	
-    ("IN-DD",	"Daman and Diu"),	
-    ("IN-DL",	"Delhi"),	
-    ("IN-LD",	"Lakshadweep"),	
-    ("IN-PY",	"Puducherry")	
+    ("IN-CH",	"Chandigarh"),
+    ("IN-DN",	"Dadra and Nagar Haveli"),
+    ("IN-DD",	"Daman and Diu"),
+    ("IN-DL",	"Delhi"),
+    ("IN-LD",	"Lakshadweep"),
+    ("IN-PY",	"Puducherry")
     )
 
 
