f8bc3b574d51d9782beeb87609f58e161c8805b2
adityacp <aditya94palaparthy@gmail.com>
2020-08-13 16:51:53 +0530

    Add string representation in teams model

--- teams/models.py
+++ teams/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from workshop_app.models import Profile
 
 class Team(models.Model):
-	members = models.ManyToManyField(Profile)
-	creator = models.OneToOneField(User, on_delete=models.CASCADE)
-	created_date = models.DateTimeField(auto_now=True)
+    members = models.ManyToManyField(Profile)
+    creator = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Team created by {self.creator.get_full_name()}"
